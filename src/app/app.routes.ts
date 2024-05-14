c7c86173c4b7042028739112270f7ea8d51d731f
kivicko <kivilcim.eray@gmail.com>
2024-05-14 07:02:28 +0200

    contact, blog, uses pages migrated to angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,18 @@
 import { Routes } from '@angular/router';
 import { BlogPostComponent } from './components/blog-post/blog-post.component';
 import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { BlogPageComponent } from './components/blog-page/blog-page.component';
+import { UsesPageComponent } from './components/uses-page/uses-page.component';
 
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'post/:slug', component: BlogPostComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'blog', component: BlogPageComponent },
+    { path: 'uses', component: UsesPageComponent },
+
+
+
 ];
---
+++ src/app/components/uses-page/uses-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-uses-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './uses-page.component.html',
+  styleUrl: './uses-page.component.css'
+})
+export class UsesPageComponent {
+
+}
